26eaf456f720936b25fa9e7f57072ae4f27da9a1
NguyenCongNam <namnguyencong23@gmail.com>
2023-10-25 12:44:17 +0700

    Layout dashboard 3

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Chat from "./Chat";
+import { Box, Stack, useTheme } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
-    <>
-      <Chat/>
-    </>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* chat */}
+      <Chat />
+      <Box
+        sx={{
+          height: "100%",
+          width: "80%",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        {/* conversation */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
