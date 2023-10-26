93c119adcea5468e2fcbf12260cc9de13cb1f983
NguyenCongNam <namnguyencong23@gmail.com>
2023-10-26 18:41:44 +0700

    message options and emoij

--- src/layouts/dashboard/SideBar.js
+++ src/layouts/dashboard/SideBar.js
@@ -1,108 +1,188 @@
-import { Avatar, Box, Divider, IconButton, Stack, useTheme } from '@mui/material'
-import React from 'react';
-import { Nav_Buttons } from "../../data";
-import { useState } from 'react';
-import useSettings from '../../hooks/useSettings';
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  useTheme,
+} from "@mui/material";
+import React from "react";
+import { Nav_Buttons, Profile_Menu } from "../../data";
+import { useState } from "react";
+import useSettings from "../../hooks/useSettings";
 import Logo from "../../assets/Images/logo.ico";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
 import AntSwitch from "../../components/AntSwitch";
 
 const SideBar = () => {
-    const theme = useTheme();
-    const [selected, setSelected] = useState(0);
-    const { onToggleMode } = useSettings();
+  // toggle mode
+  const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
+  //profile options
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box
-    p={2}
-    sx={{
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: "0 0 2px rgba(0,0,0,0.25)",
-      height: "100vh",
-    }}
-  >
-    <Stack
-      direction={"column"}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-      sx={{ height: "100%" }}
-      spacing={2}
+      p={2}
+      sx={{
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: "0 0 2px rgba(0,0,0,0.25)",
+        height: "100vh",
+      }}
     >
       <Stack
+        direction={"column"}
         alignItems={"center"}
-        spacing={4}
+        justifyContent={"space-between"}
+        sx={{ height: "100%" }}
+        spacing={2}
       >
-        <Box sx={{ backgroundColor: theme.palette.primary.main, height: 64, width: 64, borderRadius: 1.5 }} >
-          <img src={Logo} alt={"Chat app logo"} />
-        </Box>
-        <Stack
-          sx={{ width: "max-content" }}
-          direction={"column"}
-          alignItems={"center"}
-          spacing={3}
-        >
-          {Nav_Buttons && Nav_Buttons.map((el) => (
-            el.index === selected ?
+        <Stack alignItems={"center"} spacing={4}>
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt={"Chat app logo"} />
+          </Box>
+          <Stack
+            sx={{ width: "max-content" }}
+            direction={"column"}
+            alignItems={"center"}
+            spacing={3}
+          >
+            {Nav_Buttons &&
+              Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{
+                      width: "max-content",
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000"
+                          : theme.palette.text.primary,
+                    }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+            <Divider sx={{ width: "48px" }} />
+            {selected === 3 ? (
               <Box
                 p={1}
                 sx={{
                   backgroundColor: theme.palette.primary.main,
-                  borderRadius: 1.5
+                  borderRadius: 1.5,
                 }}
               >
-                <IconButton sx={{ width: "max-content", color: "#fff" }} key={el.index} >
-                  {el.icon}
+                <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                  <Gear />
                 </IconButton>
               </Box>
-              :
+            ) : (
               <IconButton
                 onClick={() => {
-                  setSelected(el.index)
+                  setSelected(3);
                 }}
                 sx={{
                   width: "max-content",
-                  color: theme.palette.mode === "light" ? "#000" : theme.palette.text.primary
+                  color:
+                    theme.palette.mode === "light"
+                      ? "#000"
+                      : theme.palette.text.primary,
                 }}
-                key={el.index}
               >
-                {el.icon}
-              </IconButton>
-          ))}
-          <Divider sx={{ width: "48px" }} />
-          {selected === 3 ?
-            <Box
-              p={1}
-              sx={{
-                backgroundColor: theme.palette.primary.main,
-                borderRadius: 1.5
-              }}
-            >
-              <IconButton sx={{ width: "max-content", color: "#fff" }} >
                 <Gear />
               </IconButton>
-            </Box>
-            :
-            <IconButton
-              onClick={() => {
-                setSelected(3)
-              }}
-              sx={{ width: "max-content", color: theme.palette.mode === "light" ? "#000" : theme.palette.text.primary }}
-            >
-              <Gear />
-            </IconButton>
-          }
+            )}
+          </Stack>
+        </Stack>
+        <Stack direction={"column"} alignItems={"center"} spacing={3}>
+          {/* Switch */}
+          <AntSwitch
+            onChange={() => {
+              onToggleMode();
+            }}
+            defaultChecked
+          />
+          <Avatar
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+            src={faker.image.avatar()}
+          />
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right"
+            }}
+            transformOrigin={{
+              vertical:"bottom",
+              horizontal: "left"
+            }}
+          >
+            <Stack spacing={1}>
+              {Profile_Menu.map((e) => (
+                <MenuItem onClick={handleClick}>
+                  <Stack
+                    sx={{ width: 100 }}
+                    direction={"row"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                  >
+                    <span>{e.title}</span>
+                    {e.icon}
+                  </Stack>
+                </MenuItem>
+              ))}
+            </Stack>
+          </Menu>
         </Stack>
       </Stack>
-      <Stack direction={"column"} alignItems={"center"} spacing={3} >
-        {/* Switch */}
-        <AntSwitch onChange={() => {
-          onToggleMode();
-        }} defaultChecked />
-        <Avatar src={faker.image.avatar()} />
-      </Stack>
-    </Stack>
-  </Box>
-  )
-}
+    </Box>
+  );
+};
 
 export default SideBar;
