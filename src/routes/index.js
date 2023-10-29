8465a8b9c3910de7896fc611967ca1a7e2e00a7e
NguyenCongNam <namnguyencong23@gmail.com>
2023-10-29 12:40:35 +0700

    call logs layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -37,6 +37,7 @@ export default function Router() {
         { path: "app", element: <GeneralApp /> },
         { path: "settings", element: <Settings /> },
         { path: "group", element: <GroupPage /> },
+        { path: "call", element: <CallPage /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -67,6 +68,10 @@ const NewPasswordPage = Loadable(
 
 const Settings = Loadable(
   lazy(() => import("../pages/dashboard/Settings")),
+);
+
+const CallPage = Loadable(
+  lazy(() => import("../pages/dashboard/Call")),
 );
 
 const GroupPage = Loadable(
---
+++ src/sections/main/StartCall.js
@@ -0,0 +1,62 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  Stack,
+} from "@mui/material";
+import React from "react";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+import { MagnifyingGlass } from "phosphor-react";
+import { CallElement } from "../../components/CallElement";
+import { MembersList } from "../../data";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const StartCall = ({ open, handleClose }) => {
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      {/* Title */}
+
+      <DialogTitle sx={{ mb: 3 }}>Start Call</DialogTitle>
+
+      {/*  */}
+
+      <DialogContent>
+        {/*  */}
+        <Stack spacing={2} >
+          <Stack sx={{ width: "100%" }}>
+            <Search>
+              <SearchIconWrapper>
+                <MagnifyingGlass color="#709CE6" />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Stack>
+          {/* Call List */}
+          {MembersList && MembersList.map((el)=> <CallElement {...el} />)}
+        </Stack>
+        
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default StartCall;
