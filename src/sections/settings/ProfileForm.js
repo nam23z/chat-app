bc44bf48dea5743ce3182097726da2b492cf1281
NguyenCongNam <namnguyencong23@gmail.com>
2023-10-29 14:27:46 +0700

    user profile & navigation

---
+++ src/sections/settings/ProfileForm.js
@@ -0,0 +1,98 @@
+import React, { useCallback } from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { Alert, Button, Stack } from "@mui/material";
+
+import { RHFTextField } from "../../components/hook-form/index";
+
+const ProfileForm = () => {
+  const ProfileSchema = Yup.object().shape({
+    name: Yup.string().required("Email is required"),
+    about: Yup.string().required("About is required"),
+    avatarUrl: Yup.string().required("Avatar is required").nullable(true),
+  });
+
+  const defaultValues = {
+    name: "",
+    about: "",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(ProfileSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const values = watch();
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue("avatarUrl", newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  const onSubmit = async (data) => {
+    try {
+      //submit data to be
+      console.log("DATA", data);
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+
+        <RHFTextField
+          name="name"
+          label="Name"
+          helperText={"This name is visbile to your contacts"}
+        />
+
+        <RHFTextField
+          multiline
+          rows={3}
+          maxRows={5}
+          name={"about"}
+          label={"About"}
+        />
+        <Stack direction={"row"} justifyContent={"end"}>
+          <Button color="primary" size="large" type="submit" variant="outlined">
+            Save
+          </Button>
+        </Stack>
+      </Stack>
+    </FormProvider>
+  );
+};
+
+export default ProfileForm;
